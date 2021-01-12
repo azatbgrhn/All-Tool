8638124d71f7d66ef5c357f19edb84c36f285de2
azatbgrhn <64813373+azatbgrhn@users.noreply.github.com>
2021-01-12 13:17:34 +0100

    site

---
+++ code.sh
@@ -0,0 +1,136 @@
+#! / bin / bash
+açık
+echo -e   "
+\ e [35 m (((#xBowtie #xBowtie)))
+"
+
+echo -e " \ e [31a "
+okumak -p " Kodunuzu yazınız: " ISLEM
+eğer [[ $ islem  ==  " insta01 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+cat Instagram / Instagram-phishing / kullanıcı adı-phishing.txt
+
+elif [[ $ islem  ==  " insta02 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+kedi Instagram / Instagram-fakepanel / kullanıcı adı-fakepanel.txt
+
+elif [[ $ islem  ==  " twitter01 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+cat Twitter / Site-phishing / user.txt
+
+elif [[ $ islem  ==  " twitter02 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+kedi Twitter / Site-fakepanel / twitter.txt
+
+elif [[ $ islem  ==  " whatsapp01 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+kedi Whatsapp / Site / code.txt
+
+elif [[ $ islem  ==  " face01 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+kedi Facebook / Site-facebook / facebook.txt
+
+elif [[ $ islem  ==  " face02 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+kedi Facebook / Site-fakepanel / fakepanel.txt
+elif [[ $ islem  ==  " mail01 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+cat Mail / Site-gmail / gmail.txt
+
+elif [[ $ islem  ==  " mail02 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+cat Mail / Site-yandex / yandex.txt
+
+elif [[ $ islem  ==  " mail03 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+cat Mail / Site-hotmail / hotmail.txt
+
+elif [[ $ islem  ==  " game01 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+kedi Oyunu / Site-wolfteam / wolfteam.txt
+
+elif [[ $ islem  ==  " game02 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+cat Game / Site-legend / legendonline.txt
+
+elif [[ $ islem  ==  " game03 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+kedi Oyunu / Site-pubg / pubg.txt
+
+elif [[ $ islem  ==  " game04 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+kedi Oyunu / Site-zula / zula.txt
+
+elif [[ $ islem  ==  " game05 " ]] ;  sonra
+uyku 1
+echo  " "
+echo -e " \ e [33mVeriler Geliyor .... "
+uyku 2
+echo -e " \ e [37a "
+kedi Oyunu / Site-lol / lol.txt
+Başka
+	açık
+        echo -e " \ e [31mYanlış Kod Girdiniz .... "
+	uyku 3
+        bash code.sh
+fi
+echo -e " \ e [31a "
+okumak -p " Ana Menüye Dönmek İçin ENTERE Başınız .... " non
+cd ..
+bash tst.sh
